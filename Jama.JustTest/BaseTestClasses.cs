0b456bae07712127b265d437cb505e91c7547ae7
jmanghera <random-spam@manghera.com>
2014-08-06 16:06:38 +0100

    Initial commit

---
+++ Jama.JustTest/BaseTestClasses.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Jama.JustTest
+{
+    public class GivenAttribute : TestFixtureSetUpAttribute { }
+
+    public class ThenAttribute : TestAttribute { }
+
+    public class ThereWasNoExceptonButOneWasExpectedException : Exception { }
+}
