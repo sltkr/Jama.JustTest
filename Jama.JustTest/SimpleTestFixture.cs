ae2b5ba23b79d45eaa42219e0c5fc05ea269efe5
sltkr <random-spam@manghera.com>
2014-08-27 15:05:53 +0100

    Added simple test fixture

---
+++ Jama.JustTest/SimpleTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Jama.JustTest
+{
+    public abstract class SimpleTestFixture
+    {
+        protected Exception CaughtException;
+        protected virtual void SetupDependencies() { }
+        protected virtual void Given() { }
+        protected virtual void When() { }
+        protected virtual void Finally() { }
+
+        [Given]
+        public void Setup()
+        {
+            SetupDependencies();
+
+            Given();
+
+            try
+            {
+                When();
+            }
+            catch (Exception exception)
+            {
+                CaughtException = exception;
+            }
+            finally
+            {
+                Finally();
+            }
+        }
+    }
+}
